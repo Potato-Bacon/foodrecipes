00b8af866e9d15a7fc60d0a3b32a0f204a56cb04
potatobacon <andylohbiz@gmail.com>
2022-08-24 17:22:15 +0800

    test api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    fetch(
+      "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=8c6f73b2&app_key=e6c45a5c0a0260c97c75738229f02eaf"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  );
 }
 
 export default App;
